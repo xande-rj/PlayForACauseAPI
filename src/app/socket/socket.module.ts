b5698b10e3145494a4ae45e97fa03e09d408f38c
Alexandre <alexandresargitario@gmail.com>
2023-12-06 20:49:42 -0300

    mudança de formato

--- src/app/socket/socket.module.ts
+++
@@ -1,9 +0,0 @@
-// socket.module.ts
-import { Module } from '@nestjs/common';
-import { SocketController } from './socket.gateway';
-
-@Module({
-  imports: [],
-  providers: [SocketController],
-})
-export class MySocketModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
-import { MySocketModule } from './app/socket/socket.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { IoAdapter } from '@nestjs/platform-socket.io';
 
 const port = process.env.PORT
 
@@ -20,19 +18,3 @@ async function bootstrap() {
 
 bootstrap();
 
-async function bootstrap2() {
-  const app = await NestFactory.create(MySocketModule);
-  const corsOptions: CorsOptions = {
-    origin: "*", // Habilita todos os origens
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-  };
-  const ioAdapter = new IoAdapter(app);
-  app.useWebSocketAdapter(ioAdapter);
-  app.enableCors(corsOptions);
-
-  await app.listen(3333);
-
-}
-
-bootstrap2();
